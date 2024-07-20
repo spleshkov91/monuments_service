f860da359e21b1ff6d872530eff5335d59c4470b
Slava Pleshkov <slavapl22@gmail.com>
2024-07-20 13:29:09 +0500

    first commit

---
+++ src/test/java/com/slavapleshkov/springboot/spring_rest/SpringRestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.slavapleshkov.springboot.spring_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/slavapleshkov/springboot/spring_rest/TestSpringRestApplication.java
@@ -0,0 +1,11 @@
+package com.slavapleshkov.springboot.spring_rest;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(SpringRestApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
